2ed1d4444860c544149ef388a0b3ddaecbc50b78
27759 <yixianghsy@gmail.com>
2019-10-02 12:29:48 +0800

    部署完成,端口28080访问

--- docker-extension/my-blog/startup.sh
+++ docker-extension/my-blog/startup.sh
@@ -4,7 +4,7 @@ set -e
 #因为要等待mysql用户和权限设置完成
 echo '等待mysql.....'
 
-sleep 25
+sleep 120
 
 echo '开始启动项目.....'
 nohup java -jar /app.jar --spring.profiles.active=docker
--- docker-extension/run.sh
+++ docker-extension/run.sh
@@ -5,15 +5,15 @@ set -e
 cd ../
 
 #如果已经创建好镜像,这一步要注释掉,避免重复操作
-mvn clean install -Dmaven.test.skip=true
+#mvn clean install -Dmaven.test.skip=true
 
 # 打包并创建镜像(不注释也可以,镜像覆盖,因此需要删除原有镜像,命令在下面,删除名称为none的镜像)
-mvn clean install -Dmaven.test.skip=true
+#mvn clean install -Dmaven.test.skip=true
 
 cd docker-extension
 
 # 停止原先运行的容器
-docker-compose stop
+    docker-compose stop
 # 删除停止的容器(如果想重启则不要执行此命令)
 docker-compose rm -f
 
